bf13add37cf4768bf3c2b41a8616ff9a3a4c9c9a
mt_tarik <skorohodit12@gmail.com>
2023-11-01 16:01:09 +0200

    NewMyPushTestDemo

--- src/components/productCard/ProductList.js
+++ src/components/productCard/ProductList.js
@@ -69,7 +69,8 @@ const ProductList = () => {
                 <div className="mb-4 flex flex-wrap">
                     <div className="mb-4">
                         <Autocomplete
-                            options={productsData.map((product) => product.name)}  // Отримати список імен з productsData
+
+                            options={productsData.map((product) => product.name)}
                             value={searchQuery}
                             onChange={(event, newValue) => {
                                 setSearchQuery(newValue);
@@ -136,20 +137,21 @@ const ProductList = () => {
                                 checked={showOnlyWithoutPhoto}
                                 onChange={() => setShowOnlyWithoutPhoto(!showOnlyWithoutPhoto)}
                             />
-
-                            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content=&quot;&quot; after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                         </label>
+
                         <span className="pl-2">Image</span>
                     </div>
                 </div>
                 {filteredProducts.slice(0, visibleProducts).map((product) => (
                     <ProductCard
-                        key={product.id} // Add a unique key here
+                        key={product.name}
                         product={product}
                         onToggleLike={() => toggleLike(product.id)}
                         onDeleteProduct={() => deleteProduct(product.id)}
                     />
                 ))}
+
 
 
                 {visibleProducts < filteredProducts.length && (
