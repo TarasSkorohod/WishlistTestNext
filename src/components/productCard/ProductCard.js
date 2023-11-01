7f03e9a7ab3845f26b21661a6691894983c3864e
mt_tarik <skorohodit12@gmail.com>
2023-11-01 15:55:07 +0200

    NewMyPushTestDemo

--- src/components/productCard/ProductCard.js
+++ src/components/productCard/ProductCard.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { IconButton } from "@mui/material";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import Image from 'next/image'; // Import Image from next/image
 
 const ProductCard = ({ product, onDeleteProduct }) => {
     const { image, productCode, name, stock, qty, price, currency } = product;
@@ -15,7 +16,7 @@ const ProductCard = ({ product, onDeleteProduct }) => {
     return (
         <div className="bg-white p-4 shadow-md mb-4">
             <div className="flex flex-col sm:flex-row items-center justify-between">
-                <img src={image} alt={name} className="w-20 h-20 sm:w-32 sm:h-32" />
+                <Image src={image} alt={name} width={220} height={220} />
                 <div className="mt-4 sm:mt-0 sm:ml-4 flex items-center">
                     <ul className="flex flex-col sm:flex-row">
                         <li className="p-2 sm:p-8">Product code: {productCode}</li>
@@ -23,7 +24,6 @@ const ProductCard = ({ product, onDeleteProduct }) => {
                         <li className="p-2 sm:p-8">Stock: {stock}</li>
                         <li className="p-2 sm:p-8">{qty} Qty</li>
                         <li className="p-2 sm:p-8"> {price} {currency}</li>
-
                     </ul>
                 </div>
                 <div className="mt-4 sm:mt-0">
@@ -35,7 +35,7 @@ const ProductCard = ({ product, onDeleteProduct }) => {
                     >
                         {liked ? <FavoriteIcon /> : <FavoriteBorderIcon />}
                     </IconButton>
-                    <IconButton  onClick={onDeleteProduct}>
+                    <IconButton onClick={onDeleteProduct}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="22" height="20" viewBox="0 0 22 20" fill="none">
                             <path d="M6 5L16 15M16 5L6 15" stroke="#303031" strokeLinecap="round" />
                         </svg>
